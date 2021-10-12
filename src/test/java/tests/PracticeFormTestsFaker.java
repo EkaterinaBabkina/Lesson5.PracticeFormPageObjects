13cb7d3fb13ce8ca8f53fbf1f8f47c40f0b6fe0c
EkaterinaBabkina <vozdux@udm.ru>
2021-10-12 19:51:54 +0400

    added Allure, Selenoid options

--- src/test/java/tests/PracticeFormTestsFaker.java
+++ src/test/java/tests/PracticeFormTestsFaker.java
@@ -9,6 +9,7 @@ import pages.RegistrationPage;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 import static tests.TestData.firstName;
 import static tests.TestData.lastName;
 
@@ -23,28 +24,55 @@ public class PracticeFormTestsFaker extends TestBase {
 
         @Test
         void selenidePracticeTestFaker() {
-                registrationPage.openPage();
-                $("#firstName").setValue(firstName);
-                $("#lastName").setValue(lastName);
-                $("#userEmail").setValue(userEmail);
-                $("#genterWrapper").$(byText("Male")).click();
-                $("#userNumber").setValue(phone);
-                $("#dateOfBirthInput").click();
-                registrationPage.calendar.setDate();
-                $("#subjectsInput").setValue("Commerce").pressEnter();
-                $("#subjectsInput").setValue("Eco").pressEnter();
-                $("#hobbiesWrapper").$(byText("Reading")).click();
-                $("#hobbiesWrapper").$(byText("Music")).click();
-                $("#uploadPicture").uploadFromClasspath("img/1.png");
-                $("#currentAddress").setValue(currentAddress);
-                $("#stateCity-wrapper").$(byText("Select State")).click();
-                $("#stateCity-wrapper").$(byText("NCR")).click();
-                $("#stateCity-wrapper").$(byText("Select City")).click();
-                $("#stateCity-wrapper").$(byText("Delhi")).click();
-                $("#submit").click();
-                $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-                $(".table-responsive").shouldHave(text(firstName + " " + lastName),
-                        text(userEmail), text("Male"), text(phone), text("06 October,1993"),
-                        text("Commerce, Economics"), text("Reading, Music"), text("1.png"), text(currentAddress), text("NCR Delhi"));
+                step("Открытие регистрационной формы", () -> {
+                        registrationPage.openPage();
+                        });
+                step("Ввод имени студента", () -> {
+                        $("#firstName").setValue(firstName);
+                        $("#lastName").setValue(lastName);
+                });
+                step("Ввод почты студента", () -> {
+                        $("#userEmail").setValue(userEmail);
+                });
+                step("Выбор пола студента", () -> {
+                        $("#genterWrapper").$(byText("Male")).click();
+                });
+                step("Ввод номера телефона студента", () -> {
+                        $("#userNumber").setValue(phone);
+                });
+                step("Ввод даты рождения студента", () -> {
+                        $("#dateOfBirthInput").click();
+                        registrationPage.calendar.setDate();
+                });
+                step("Ввод предметов студента", () -> {
+                        $("#subjectsInput").setValue("Commerce").pressEnter();
+                        $("#subjectsInput").setValue("Eco").pressEnter();
+                });
+                step("Ввод хобби студента", () -> {
+                        $("#hobbiesWrapper").$(byText("Reading")).click();
+                        $("#hobbiesWrapper").$(byText("Music")).click();
+                });
+                step("Загрузка фотографии студента", () -> {
+                        $("#uploadPicture").uploadFromClasspath("img/1.png");
+                });
+                step("Ввод адреса студента", () -> {
+                        $("#currentAddress").setValue(currentAddress);
+                        $("#stateCity-wrapper").$(byText("Select State")).click();
+                        $("#stateCity-wrapper").$(byText("NCR")).click();
+                        $("#stateCity-wrapper").$(byText("Select City")).click();
+                        $("#stateCity-wrapper").$(byText("Delhi")).click();
+                });
+                step("Отправка формы", () -> {
+                        $("#submit").click();
+                });
+
+                step("Проверка формы", () -> {
+                        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+                        $(".table-responsive").shouldHave(text(firstName + " " + lastName),
+                                text(userEmail), text("Male"), text(phone), text("06 October,1993"),
+                                text("Commerce, Economics"), text("Reading, Music"), text("1.png"), text(currentAddress), text("NCR Delhi"));
+                });
+
+
         }
 }
